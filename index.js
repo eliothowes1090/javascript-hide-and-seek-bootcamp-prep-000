6b2e2302570763737bd20b9b81bc6534d5baeca5
Eliot Howes <ejhowes@gmail.com>
2019-07-14 13:24:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function getFirstSelector(selector) {
+  var firstSelector = document.querySelector(selector)
+  return firstSelector
+}
 
+function nestedTarget() {
+  var nestedTarget = document.getElementById('nested').getElementsByClassName('target')[0]
+  return nestedTarget
+}
+
+function increaseRankBy(n) {
+  
+}
